96c69f9f2e5102f740010edd59e8c3d0004deca1
bordanattila <bordan_attila@hotmail.com>
2023-09-23 06:54:42 -0400

    added hover effect to navbar

--- src/components/Navigation.js
+++ src/components/Navigation.js
@@ -5,22 +5,29 @@ import { Nav, Navbar, Container, Stack } from "react-bootstrap";
 
 function NavigationBar() {
     return (
-         <Container>
-        <Navbar id="nav" fixed="top" className="navigationbar" expand="lg" >
-
-            <Navbar.Brand>bordan attila</Navbar.Brand>
-            <Navbar.Toggle aria-controls="responsive-navbar-nav" />
-            <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-end">
-                {/* <Nav className="me-auto"> */}
-                <Stack direction="horizontal" gap={5}>
-                    <Nav.Link href="#home">./home</Nav.Link>
-                    <Nav.Link href="#skills">./skills</Nav.Link>
-                    <Nav.Link href="#projects">./projects</Nav.Link>
-                    <Nav.Link href="#contact">./contact</Nav.Link>
-                </Stack>
-                {/* </Nav> */}
-            </Navbar.Collapse>
-        </Navbar>
+        <Container>
+            <Navbar collapseOnSelect id="nav" fixed="top" className="navigationbar" expand="lg" >
+                <Navbar.Brand href="#home">bordán attila</Navbar.Brand>
+                <Navbar.Toggle aria-controls="responsive-navbar-nav" />
+                <Navbar.Collapse id="responsive-navbar-nav" className="justify-content-around">
+                    <Nav className="" activeKey="/home" variant="underline">
+                        {/* <Stack direction="horizontal" gap={5}> */}
+                        <Nav.Item>
+                            <Nav.Link href="#home">./home</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#skills">./skills</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#projects">./projects</Nav.Link>
+                        </Nav.Item>
+                        <Nav.Item>
+                            <Nav.Link href="#contact">./contact</Nav.Link>
+                        </Nav.Item>
+                        {/* </Stack> */}
+                    </Nav>
+                </Navbar.Collapse>
+            </Navbar>
         </Container>
     );
 }
